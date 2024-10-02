95e716df264c0aa54ef9f68cb45a8fa9fe100c0b
emidiojoao <143660761+emidiojoao@users.noreply.github.com>
2024-10-01 21:06:53 -0300

    inicializando SA e adicionando dependencias

---
+++ ProjetoSaIot/src/main/java/edu/senai/br/projetosaiot/ProjeyoSaIot.java
@@ -0,0 +1,12 @@
+package edu.senai.br.projetosaiot;
+
+/**
+ *
+ * @author joao_c_emidio
+ */
+public class ProjeyoSaIot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
